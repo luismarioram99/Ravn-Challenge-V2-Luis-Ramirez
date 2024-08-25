0f41b0d8ebf8e808e31616273179d70762d45917
Luis Ramirez <luismarioram99@gmail.com>
2024-08-25 13:50:21 -0600

    ✨ Add database integration
    Create ormconfig and include typeorm in the projects imported modules

---
+++ ormconfig.ts
@@ -0,0 +1,20 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+
+// Cargar variables de entorno
+config();
+
+const ormconfig: TypeOrmModuleOptions = {
+  type: 'postgres', // O el tipo de base de datos que uses
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 5432),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: process.env.DB_SYNC === 'true', // Solo para desarrollo
+};
+
+console.info(ormconfig);
+
+export default ormconfig;
